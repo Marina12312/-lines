f7363b3ce954dd4117babc95c9b95a1e8613e149
Admin <kislovv95@mail.ru>
2022-12-28 12:36:10 +0300

    задачи часть1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+        System.out.println("задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName =" Ivanov";
+        String fullName = lastName+" "+ firstName+" "+ middleName;
+        System.out.println(fullName);
+    }
+
+    private static void task2() {
+        System.out.println("задача2");
+        String fullName = " ivanov ivan ivanovach";
+        System.out.println(fullName.toUpperCase());
+
+
+    }
+
+    private static void task3() {
+        System.out.println("Задача3");
+        String fullName ="Иванов Семён Семёнович";
+        String newName = fullName.replace('ё', 'e');
+        System.out.println(newName);
+
     }
 }
